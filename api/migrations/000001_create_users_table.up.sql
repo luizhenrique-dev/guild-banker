d25899248f5c8b6f66cc9fe820b5e73d9fdb6037
Luiz Henrique <luizhenrique@zgsolucoes.com.br>
2026-04-29 00:17:27 -0300

    feat(user): update user table to include updated_by column

--- api/migrations/000001_create_users_table.up.sql
+++ api/migrations/000001_create_users_table.up.sql
@@ -4,10 +4,11 @@ CREATE TABLE users
 	external_id TEXT        NOT NULL UNIQUE,
 	name        TEXT        NOT NULL,
 	email       TEXT        NOT NULL UNIQUE,
+	enabled     BOOLEAN     NOT NULL        DEFAULT TRUE,
 	created_at  TIMESTAMPTZ NOT NULL        DEFAULT NOW(),
 	created_by  TEXT        NOT NULL,
-	enabled     BOOLEAN     NOT NULL        DEFAULT TRUE,
 	updated_at  TIMESTAMPTZ,
+	updated_by  TEXT,
 	disabled_at TIMESTAMPTZ,
 	disabled_by TEXT
 );
